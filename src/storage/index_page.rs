d6ffea68e2665f699a2a45cf2d468cd2f115f12e
Anish Sinha <anishsinha0128@gmail.com>
2022-09-22 01:48:55 -0400

    Add implementations for ioutil and fs functions

---
+++ src/storage/index_page.rs
@@ -0,0 +1,8 @@
+// SOURCES + USEFUL LINKS
+// https://github.com/cmu-db/bustub/blob/master/src/include/storage/page/b_plus_tree_page.h
+#![allow(dead_code, unused_imports)]
+
+use crate::shared::PageId;
+use crate::storage::objptr::ObjectPtr;
+
+pub struct IndexPage {}
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 pub mod bufmgr;
 mod fs;
+mod index_page;
 mod ioutil;
 mod lrureplacer;
+mod objptr;
 mod page;
