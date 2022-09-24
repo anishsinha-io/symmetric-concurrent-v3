fd9c4f62a8546dd08d1fdc25c73b6ec6d4ebaf89
Anish Sinha <anishsinha0128@gmail.com>
2022-09-24 08:00:44 -0400

    Create  struct

--- src/storage/diskmgr.rs
+++ src/storage/diskmgr.rs
@@ -1,7 +1,17 @@
 #![allow(dead_code, unused_imports)]
 
+use std::fs::File;
+use std::sync::Arc;
+
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
-use crate::storage::ioutil::{decode, encode};
-
 use crate::concurrency::{acquire, release, Synchronized};
+use crate::storage::fsutil::{read_bytes, write_bytes};
+use crate::storage::ioutil::{decode, encode, from_buffer, to_buffer};
+
+pub struct DiskMgr {
+    db_handle: Synchronized<File>,
+    log_handle: Synchronized<File>,
+}
+
+impl DiskMgr {}
