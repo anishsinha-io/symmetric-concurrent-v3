603df650c7d4a430b18cc8dd67d6ee75da555dcf
Anish Sinha <anishsinha0128@gmail.com>
2022-09-23 20:21:57 -0400

    Finish ioutil module + tests

--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -1,5 +1,8 @@
 #![allow(dead_code)]
 
+use std::env;
+use std::path::PathBuf;
+
 pub type FrameId = isize;
 pub type PageId = isize;
 pub type Oid = u16;
@@ -8,3 +11,7 @@ pub const HEADER_ID: usize = 0;
 pub const PAGE_SIZE: usize = 4096;
 pub const INVALID_FRAME_ID: isize = -1;
 pub const INVALID_PAGE_ID: isize = -1;
+
+pub fn cwd() -> std::io::Result<PathBuf> {
+    env::current_dir()
+}
